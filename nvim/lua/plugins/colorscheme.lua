fa07628a257488044677f403675579493bd0e130
Toyesh2907 <kunwartoyesh@gmail.com>
2026-02-12 10:36:09 +0530

    chore: updated the neovim config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,44 +1,90 @@
-return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        opts = {
-            flavour = "macchiato", -- latte, frappe, macchiato, mocha
-            transparent_background = true,
+-- Set your preferred colorscheme here
+local COLORSCHEME = "tokyodark" -- Options: "catppuccin", "gruvbox", "onedark", "tokyodark", "bamboo"
 
-            integrations = {
-                telescope = true,
-                native_lsp = {
-                    enabled = true,
+local colorschemes = {
+    catppuccin = {
+        {
+            "catppuccin/nvim",
+            name = "catppuccin",
+            opts = {
+                flavour = "macchiato",
+                transparent_background = true,
+                integrations = {
+                    telescope = true,
+                    native_lsp = {
+                        enabled = true,
+                    },
                 },
             },
         },
     },
-
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "catppuccin",
+    gruvbox = {
+        {
+            "ellisonleao/gruvbox.nvim",
+            opts = {
+                contrast = "soft",
+                transparent_mode = true,
+                integrations = {
+                    telescope = true,
+                    native_lsp = true,
+                },
+            },
+        },
+    },
+    onedark = {
+        {
+            "navarasu/onedark.nvim",
+            priority = 1000,
+            opts = {
+                style = "darker",
+                transparent = true,
+                term_colors = true,
+            },
+        },
+    },
+    tokyodark = {
+        {
+            "tiagovla/tokyodark.nvim",
+            priority = 1000,
+            opts = {
+                transparent_background = true,
+                gamma = 1.0,
+            },
+        },
+    },
+    bamboo = {
+        {
+            "ribru17/bamboo.nvim",
+            priority = 1000,
+            opts = {},
         },
     },
 }
--- return {
---     {
---         "ellisonleao/gruvbox.nvim",
---         opts = {
---             contrast = "soft", -- hard, soft, or empty
---             transparent_mode = true,
---             integrations = {
---                 telescope = true,
---                 native_lsp = true,
---             },
---         },
---     },
---
---     {
---         "LazyVim/LazyVim",
---         opts = {
---             colorscheme = "gruvbox",
---         },
---     },
--- }
+
+-- Get the selected colorscheme configuration
+local selected = colorschemes[COLORSCHEME] or colorschemes.bamboo
+
+-- Add LazyVim colorscheme configuration
+table.insert(selected, {
+    "LazyVim/LazyVim",
+    opts = {
+        colorscheme = COLORSCHEME,
+    },
+})
+
+-- Add transparent cursorline configuration
+table.insert(selected, {
+    "LazyVim/LazyVim",
+    opts = function(_, opts)
+        -- Set up an autocmd to make cursorline transparent after colorscheme loads
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            pattern = "*",
+            callback = function()
+                vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" })
+                vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "NONE" })
+            end,
+        })
+    end,
+})
+
+return selected
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -3,8 +3,18 @@ return {
         "folke/snacks.nvim",
         opts = {
             picker = {
-                hidden = true, -- show dotfiles
-                ignored = true, -- show .gitignored files
+                sources = {
+                    files = {
+                        -- show dotfiles
+                        hidden = true,
+                        -- show gitignored files too
+                        ignored = true,
+                        -- follow symlinks (optional)
+                        follow = true,
+                        -- optionally exclude only .git folder
+                        exclude = { ".git", "node_modules" },
+                    },
+                },
             },
         },
     },
